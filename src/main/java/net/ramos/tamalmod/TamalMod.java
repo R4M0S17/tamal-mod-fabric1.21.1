21671aaa6bd34229b3e5fcf717932cf08c9eecf9
Ramos <javiramosgt@gmail.com>
2026-04-18 22:29:32 -0400

    Initial commit

---
+++ src/main/java/net/ramos/tamalmod/TamalMod.java
@@ -0,0 +1,17 @@
+package net.ramos.tamalmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TamalMod implements ModInitializer {
+	public static final String MOD_ID = "tamal-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/ramos/tamalmod/TamalModClient.java
@@ -0,0 +1,10 @@
+package net.ramos.tamalmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TamalModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
